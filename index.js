f9b9ea2559a68502ac0b8147f30863e3699e7d3f
Tobias Gurtzick <magic@wizardtales.com>
2022-05-20 15:07:55 +0200

    adjusted
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
-var https = require('https');
+let https = require('https');
+const moment = require('moment');
 
-var ovh = require('ovh')({
+let ovh = require('ovh')({
   endpoint: process.env.APP_ENDPOINT,
   appKey: process.env.APP_KEY,
   appSecret: process.env.APP_SECRET,
   consumerKey: process.env.CONSUMER_KEY
 });
 
-var array_index = 0;
-var invoices = [];
+let array_index = 0;
+let invoices = [];
 
 // This method allow to work synchronously and avoid flood and timeout issue
 function getInfos(array_index, invoices) {
   if (array_index < invoices.length) {
     invoice = invoices[array_index];
-    ovh.request('GET', '/me/bill/' + invoice, function (err, file) {
+    ovh.request('GET', `/me/bill/${invoice}`, function (err, file) {
       if (err == null) {
         console.log(
           invoice +
@@ -33,6 +34,10 @@ function getInfos(array_index, invoices) {
   }
 }
 
-ovh.request('GET', '/me/bill', function (err, invoices) {
-  getInfos(array_index, invoices);
-});
+ovh.request(
+  'GET',
+  `/me/bill?date.from=${moment().subtract(30, 'days').format('YYYYMMDD')}`,
+  function (err, invoices) {
+    getInfos(array_index, invoices);
+  }
+);
