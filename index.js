a2a922e0273b8b3aef936d1eb7fa9e723646962d
Tobias Gurtzick <magic@wizardtales.com>
2023-12-25 10:44:18 +0100

    further improvements
    Signed-off-by: Tobias Gurtzick <magic@wizardtales.com>

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import moment from 'moment';
 import { promises as fs } from 'fs';
 import Promise from 'bluebird';
@@ -7,19 +6,22 @@ import got from 'got';
 import OVH from 'ovh';
 import { FormData, File, Blob } from 'formdata-node';
 import { fileFromPath } from 'formdata-node/file-from-path';
+import Module from 'node:module';
+dotenv.config();
+const require = Module.createRequire(import.meta.url);
+const createdInvoices = require('./invoices.json');
 
-let ovh = OVH({
+const ovh = OVH({
   endpoint: process.env.APP_ENDPOINT,
   appKey: process.env.APP_KEY,
   appSecret: process.env.APP_SECRET,
   consumerKey: process.env.CONSUMER_KEY
 });
 
-let array_index = 0;
-let invoices = [];
+const invoices = [];
 
 // This method allow to work synchronously and avoid flood and timeout issue
-function getInfos(invoice) {
+function getInfos (invoice) {
   return ovh.requestPromised('GET', `/me/bill/${invoice}`);
 }
 
@@ -27,13 +29,22 @@ const auth = { Authorization: `Bearer ${process.env.LEXOFFICE}` };
 
 ovh.request(
   'GET',
-  `/me/bill?date.from=${moment().subtract(1, 'days').toISOString()}`,
+  `/me/bill?date.from=${moment().subtract(70, 'days').toISOString()}`,
   function (err, invoices) {
+    if (err) {
+      throw err;
+    }
     Promise.resolve(invoices)
-      .map(getInfos)
+      .map(getInfos, { concurrency: 2 })
       .map(async x => {
         const pdf = await got(x.pdfUrl, { responseType: 'buffer' });
         return { ...x, pdf: pdf.body };
+      }, { concurrency: 2 }).filter(x => {
+        if (createdInvoices[x.billId]) {
+          return false;
+        }
+
+        return true;
       })
       .map(async x => {
         await fs.writeFile(`/tmp/__billxm__${x.billId}.pdf`, x.pdf);
@@ -41,7 +52,7 @@ ovh.request(
         const form = new FormData();
         form.set('file', await fileFromPath(`/tmp/__billxm__${x.billId}.pdf`));
         form.set('type', 'voucher');
-        //return client.uploadFile(form);
+        // return client.uploadFile(form);
 
         // not available...
         // const voucher = await got('https://api.lexoffice.io/v1/vouchers', {
@@ -70,20 +81,31 @@ ovh.request(
         //   }
         // });
 
-        return got(
-          //`https://api.lexoffice.io/v1/vouchers/${voucher.body.id}/files`,
+        const res = await got(
+          // `https://api.lexoffice.io/v1/vouchers/${voucher.body.id}/files`,
           'https://api.lexoffice.io/v1/files',
           {
             method: 'POST',
             headers: auth,
-            body: form
+            body: form,
+            retry: {
+              limit: 3
+            }
           }
         );
-      })
+
+        createdInvoices[x.billId] = 1;
+
+        return res;
+      }, { concurrency: 2 })
       .catch(x => {
         console.log(x);
         console.log('h', x.response.body);
       })
-      .then(console.log);
+      .then(x => {
+        console.log(x, JSON.stringify(createdInvoices));
+
+        return fs.writeFile('./invoices.json', JSON.stringify(createdInvoices), 'utf8');
+      });
   }
 );
